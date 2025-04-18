3679e974ce25f35006b6167e30f6d7e71a73e688
manato1201 <manato1201m@icloud.com>
2025-04-18 12:40:16 +0900

    add:トランジションシェーダー追加

---
+++ Assets/Scripts/DB/WordImage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DB {
+
+    [CreateAssetMenu(fileName = "WordImage", menuName = "Scriptable Objects/WordImage")]
+    public class WordImage : ScriptableObject
+    {
+
+        #region QOL向上処理
+        // CakeParamsSOが保存してある場所のパス
+        public const string PATH = "DB_Item";
+
+        // CakeParamsDBの実体
+        private static WordImage _entity = null;
+        public static WordImage Entity
+        {
+            get
+            {
+                // 初アクセス時にロードする
+                if (_entity == null)
+                {
+                    _entity = Resources.Load<WordImage>(PATH);
+
+                    //ロード出来なかった場合はエラーログを表示
+                    if (_entity == null)
+                    {
+                        Debug.LogError(PATH + " not found");
+                    }
+                }
+
+                return _entity;
+            }
+        }
+        #endregion
+
+        [Header("ワードスプライト")] public List<NameSprite> ItemSprites;
+    }
+    [Serializable]
+    public class NameSprite
+    {
+        public string Name;
+        public Sprite[] Sprite;
+    }
+}
